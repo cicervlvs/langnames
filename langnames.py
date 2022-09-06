9144a62840a4122395df54b50afd8d9bcf9e2fd5
Alejandro García <alejandrogarciaag41@gmail.com>
2022-09-06 20:27:46 +0200

    Change langs.py to follow def syntax

--- langnames.py
+++ langnames.py
@@ -118,16 +118,16 @@ langs.family_glot = glotfams_fixed
 #make the keys for the latex package
 with open("latex/ln_langs_glot.tex", "w") as lang_keys_glot:
     for name, code in zip(langs.name_glot, langs.iso639P3code):
-        lang_keys_glot.write(f"\\define@key{{names}}{{{code}}}{{{name}}}\n")
+        lang_keys_glot.write(f"\\def\\langnames@names@glot@{code}{{{name}}}\n")
 
 with open("latex/ln_langs_wals.tex", "w") as lang_keys_wals:
     for name, code in zip(langs.name_wals, langs.iso639P3code):
-        lang_keys_wals.write(f"\\define@key{{names}}{{{code}}}{{{name}}}\n")
+        lang_keys_wals.write(f"\\def\\langnames@names@wals@{code}{{{name}}}\n")
 
 with open("latex/ln_fams_glot.tex", "w") as fam_keys_glot:
     for fam, code in zip(langs.family_glot, langs.iso639P3code):
-        fam_keys_glot.write(f"\\define@key{{fams}}{{{code}}}{{{fam}}}\n")
+        fam_keys_glot.write(f"\\def\\langnames@fams@glot@{code}{{{fam}}}\n")
 
 with open("latex/ln_fams_wals.tex", "w") as fam_keys_wals:
     for fam, code in zip(langs.family_wals, langs.iso639P3code):
-        fam_keys_wals.write(f"\\define@key{{fams}}{{{code}}}{{{fam}}}\n")
+        fam_keys_wals.write(f"\\def\\langnames@fams@wals@{code}{{{fam}}}\n")
