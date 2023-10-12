f98300ceb05656a8a937bca0862fd56d0661332a
Alejandro García <alejandrogarciaag41@gmail.com>
2023-10-12 19:09:32 +0200

    Make string regex to avoid error

--- langnames.py
+++ langnames.py
@@ -72,13 +72,14 @@ duplicate_codes = code_n[code_n > 1].reset_index()
 
 # get duplicate codes the names of which have parentheses (dialect suspects)
 langs_with_pars = langs[langs.name_glot.str.contains(" \(", na=False) == True]
+print(langs_with_pars[:5])
 dupes_with_pars = langs_with_pars[
     langs_with_pars["iso639P3code"].isin(duplicate_codes["index"])
 ]
 
 # get names (without parentheses), as well as number of them
 sus_names = {}
-suspect_pattern = re.compile("(.+) \(")
+suspect_pattern = re.compile(r"(.+) \(")
 
 for name, code in zip(dupes_with_pars["name_glot"], dupes_with_pars["iso639P3code"]):
     main_name = suspect_pattern.match(name).group(1)
